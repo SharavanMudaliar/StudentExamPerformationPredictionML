93076ad9529ca57b5c83cbfd06b5556f5dadfaed
SharavanMudaliar <mudaliarsharavan@gmail.com>
2024-12-07 01:35:29 +0530

    Final Test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -24,13 +24,18 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('writing_score')),  # reading_score and writing_score were swapped
+            writing_score=float(request.form.get('reading_score'))   # Ensure this reflects the correct input fields
         )
         pred_df = data.get_data_as_data_frame()
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
         return render_template('home.html', results=results[0])
+
+# Route for the About page
+@app.route('/about')
+def about():
+    return render_template('about.html')  # Render the about.html template
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
